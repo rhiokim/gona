ae94d69ec2c3f125ea319adf086c8e2d45c08985
rhiokim <rhio.kim@gmail.com>
2017-05-07 16:25:44 +0900

    style(tasks): text decoration of completed task

--- src/components/tasks/CompletedTasks.jsx
+++ src/components/tasks/CompletedTasks.jsx
@@ -5,17 +5,33 @@ import {connect} from 'react-redux'
 import * as Actions from '../../actions/tasks'
 
 class CompletedTasks extends React.Component {
+  handleClick () {}
+
   render () {
+    const tasks = this.props.completedTasks
     return (
       <ul className="list">
-        <li className="task">
-          <label className="task-label">completed tasks</label>
-        </li>
+        {tasks.map((task, i) => (
+          <li className="task done" key={i}>
+            <label className="task-label">
+              <input
+                type="checkbox"
+                name=""
+                checked={task.done}
+                onChange={this.handleClick.bind(this, i)}
+              />
+              <span>{task.subject}</span>
+            </label>
+          </li>
+        ))}
       </ul>
     )
   }
 }
 
-const mapStateToProps = state => ({})
+const mapStateToProps = state => ({
+  completedTasks: state.App.completedTasks
+})
 const mapDispatchToProps = dispatch => bindActionCreators(Actions, dispatch)
+
 export default connect(mapStateToProps, mapDispatchToProps)(CompletedTasks)
