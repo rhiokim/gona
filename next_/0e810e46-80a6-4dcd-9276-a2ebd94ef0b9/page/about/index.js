a9949b68d1bb247bc16df9870d39588dee3674f5
rhiokim <rhio.kim@gmail.com>
2017-05-20 23:33:59 +0900

    Updates

--- next_/0e810e46-80a6-4dcd-9276-a2ebd94ef0b9/page/about/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/about', function() {
-            var comp = module.exports=webpackJsonp([2],[],[213]);
-            return { page: comp.default }
-          })
-        
