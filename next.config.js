f19494d2acd5c4fe42f131bb2a435df692a4b904
rhiokim <rhio.kim@gmail.com>
2017-05-20 12:16:37 +0900

    initial commit

---
+++ next.config.js
@@ -0,0 +1,4 @@
+exports.exportPathMap = () => ({
+  "/": { page: "/" },
+  "/about": { page: "/about" }
+})
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import Head from '../components/head'
+import Nav from '../components/nav'
+
+export default () => (
+  <div>
+    <Head title="Home" />
+    <Nav />
+
+    <h1>Welcome to next.js!!</h1>
+  </div>
+)
