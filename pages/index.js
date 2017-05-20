4019383ba2e84103f14165fb17cd43f684d4c388
rhiokim <rhio.kim@gmail.com>
2017-05-20 13:25:22 +0900

    feat: add skeleton css lib

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
-import Link from 'next/link'
-import Head from '../components/head'
-import Nav from '../components/nav'
+import Link from 'next/link';
+import Head from '../components/head';
+import Nav from '../components/nav';
 
 export default () => (
   <div>
     <Head title="Home" />
     <Nav />
 
-    <h1>Welcome to next.js!!</h1>
+    <div className="container">
+      <div className="row">
+        <div className="one-half column"style={{marginTop: "25%"}}>
+          <h4>Basic Page</h4>
+          <p>
+            This index.html page is a placeholder with the CSS, font and favicon. It's just waiting for you to add some content! If you need some help hit up the
+            {' '}
+            <a href="http://www.getskeleton.com">Skeleton documentation</a>
+            .
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
-)
+);
