9ee05664a04e2b38fae094bb40345ae8d66ed27a
rhiokim <rhio.kim@gmail.com>
2017-05-21 17:05:11 +0900

    Updates

---
+++ next_/d8d62161-74e6-4326-b36b-c9f383342bde/page/help/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/help', function() {
+            var comp = module.exports=webpackJsonp([6],{165:function(e,t,l){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var a=l(2),u=n(a),c=l(85),r=n(c);t.default=function(){return u.default.createElement(r.default,null,u.default.createElement("div",{className:"container"},u.default.createElement("div",{className:"section center"},u.default.createElement("img",{src:"/static/images/sloth.png",width:"50"})," ",u.default.createElement("br",null),"coming up slowly...")))}},453:function(e,t,l){e.exports=l(165)}},[453]);
+            return { page: comp.default }
+          })
+        
